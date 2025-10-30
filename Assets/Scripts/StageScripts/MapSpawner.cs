c4db7e3549e1d3e1b718293a7b606ae6653a9c5a
松岡　美伶 <hmj2422026@stu.o-hara.ac.jp>
2025-10-30 11:23:52 +0900

    マップ選択できるようにした

--- Assets/Scripts/StageScripts/MapSpawner.cs
+++ Assets/Scripts/StageScripts/MapSpawner.cs
@@ -1,46 +1,59 @@
 using UnityEngine;
 using System.Collections.Generic;
+
 public class MapSpawner : MonoBehaviour
 {
-	[SerializeField] private GameObject[] mapPrefabs; // 4つのマップ
-	[SerializeField] private float mapWidth = 50f;    // マップの幅
+	//ゲームシーン側
+	[SerializeField] private MapGroup[] mapGroups;
+	[SerializeField] private float mapWidth = 90f;
 
 	private List<GameObject> activeMaps = new List<GameObject>();
-	private int nextMapIndex = 0; // 次に出すマップの番号
-	private float spawnX = 0f;    // 次に生成する位置
+	private float spawnX = 0f;
+	private int selectedIndex;
+
+	[System.Serializable]
+	public class MapGroup
+	{
+		public GameObject[] mapVariants;
+	}
 
 	void Start()
 	{
-		// 最初に4枚並べる
-		for (int i = 0; i < mapPrefabs.Length; i++)
+		selectedIndex = MapSelectUI.SelectedMapIndex;
+
+		if (selectedIndex < 0 || selectedIndex >= mapGroups.Length)
 		{
-			SpawnMap(i);
+			Debug.LogError("マップ選択が無効です。");
+			return;
+		}
+
+		// 最初に4枚並べる
+		for (int i = 0; i < 4; i++)
+		{
+			int variant = i % mapGroups[selectedIndex].mapVariants.Length;
+			SpawnMap(selectedIndex, variant);
 		}
 	}
 
 	void Update()
 	{
-		// 左側のマップが画面外に出たら削除して次を生成
-		if (activeMaps.Count > 0)
-		{
-			GameObject firstMap = activeMaps[0];
-			if (firstMap.transform.position.x + mapWidth < Camera.main.transform.position.x - 30f)
-			{
-				// 1番左のマップ削除
-				Destroy(firstMap);
-				activeMaps.RemoveAt(0);
+		if (activeMaps.Count == 0) return;
 
-				// 次のマップ生成（ループ）
-				nextMapIndex = (nextMapIndex + 1) % mapPrefabs.Length;
-				SpawnMap(nextMapIndex);
-			}
+		GameObject firstMap = activeMaps[0];
+		if (firstMap.transform.position.x + mapWidth < Camera.main.transform.position.x - 30f)
+		{
+			Destroy(firstMap);
+			activeMaps.RemoveAt(0);
+
+			int nextVariant = Random.Range(0, mapGroups[selectedIndex].mapVariants.Length);
+			SpawnMap(selectedIndex, nextVariant);
 		}
 	}
 
-	void SpawnMap(int prefabIndex)
+	void SpawnMap(int mapIndex, int variantIndex)
 	{
-		GameObject prefab = mapPrefabs[prefabIndex];
-		GameObject map = Instantiate(prefab, new Vector3(spawnX, 0, 0), prefab.transform.rotation);
+		GameObject prefab = mapGroups[mapIndex].mapVariants[variantIndex];
+		GameObject map = Instantiate(prefab, new Vector3(spawnX, 0, 0), Quaternion.identity);
 		activeMaps.Add(map);
 		spawnX += mapWidth;
 	}
