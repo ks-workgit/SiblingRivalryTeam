cec0e01029cbe1e9fa3549761aa3fe9235422bb9
ks-workgit <204952982+Nabeyama0806@users.noreply.github.com>
2025-10-31 15:12:52 +0900

    同時に被弾した時に攻撃できなくなるのを修正

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -165,7 +165,7 @@ public class PlayerController : MonoBehaviour
         if (!m_isGrounded || m_isGuard || m_isStun || m_isAvoidance || m_isAttacking) return;
 
         m_canMove = false;
-		m_animator.SetFloat("AttackSpeed", m_characterManager.GetSetAtttackSpeed);
+        m_animator.SetFloat("AttackSpeed", m_characterManager.GetSetAtttackSpeed);
         m_animator.SetTrigger("Attack");
     }
 
@@ -275,6 +275,7 @@ public class PlayerController : MonoBehaviour
         m_invincibleTimer = InvincibleTime;
         m_isStun = isStun;
         m_isInvincible = true;
+        m_isAttacking = false;
         m_animator.SetTrigger("Stun");
     }
 
