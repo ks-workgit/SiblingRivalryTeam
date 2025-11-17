5bd2ec468f9ae63e69948f70779cace986f536c5
松岡　美伶 <hmj2422026@stu.o-hara.ac.jp>
2025-11-17 10:57:54 +0900

    ToDoリスト更新

---
+++ Assets/Scripts/StageScripts/GoalTrigger.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalTrigger : MonoBehaviour
+{
+	[SerializeField] private int playerNumber;
+	[SerializeField] SelectCharacter selectCharacter;
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
