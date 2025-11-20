bc7281ed565f6e90856833f13a3a9c8d1dc146b8
ks-workgit <204952982+Nabeyama0806@users.noreply.github.com>
2025-11-20 10:27:46 +0900

    ToDoリスト更新

--- Assets/Scripts/System/RuleManager.cs
+++ Assets/Scripts/System/RuleManager.cs
@@ -8,7 +8,7 @@ public class RuleManager : MonoBehaviour
 
     public static RuleSettingsData CurrentRule;
 
-    [SerializeField] private RuleSettingsData m_ruleSettingsData;
+    [SerializeField] RuleSettingsData m_ruleSettingsData;
 
     private void Awake()
     {
