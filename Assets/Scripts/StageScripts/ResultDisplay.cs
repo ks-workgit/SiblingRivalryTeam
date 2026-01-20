20bea9991840ee8350cb8e4e23b70ae5424ab54c
松岡　美伶 <hmj2422026@stu.o-hara.ac.jp>
2026-01-20 12:12:29 +0900

    playerを表示させた、順位反映作成中

---
+++ Assets/Scripts/StageScripts/ResultDisplay.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultDisplay : MonoBehaviour
+{
+	[Header("順位表示")]
+	[SerializeField] private List<GameObject> rankedPlayers;
+
+	[Header("位置表示")]
+	[SerializeField] private Transform[] rankePositions;
+
+    void Start()
+    {
+		ShowReesut();   
+    }
+
+    // Update is called once per frame
+    private void ShowReesut()
+	{
+		for (int i = 0; i < rankedPlayers.Count && i < rankePositions.Length; i++)
+		{
+			GameObject player = rankedPlayers[i];
+
+			player.transform.position = rankePositions[i].position;
+			player.transform.rotation = rankePositions[i].rotation;
+
+			var controller = player.GetComponent<PlayerController>();
+			if(controller != null)
+				controller.enabled = false;
+
+			var rd = player.GetComponent<Rigidbody>();
+			if(rd != null)
+			{
+				rd.velocity = Vector3.zero;
+				rd.isKinematic = true;
+			}
+
+			/*
+			//アニメーション用
+			 var anim = player.GetComponent<Animator>();
+			if (anim != null)
+				anim.Play("Result");*/
+		}
+	}
+}
